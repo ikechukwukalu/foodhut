07c6a09fe63f2766a6734f620ac7645a8766471b
Monitecture <100952206+monitecture@users.noreply.github.com>
2023-04-22 15:49:29 +0100

    Fix AWS redis connection

--- config/database.php
+++ config/database.php
@@ -120,31 +120,38 @@ return [
     */
 
     'redis' => [
+        'cluster' => env('REDIS_CLUSTER_ENABLED', true),
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
-            // 'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'clusters' => [
-
             'default' => [
-                'scheme' => 'tls', //Fixed AWS Redis connection
-                'url' => env('REDIS_URL'),
                 'host' => env('REDIS_HOST', '127.0.0.1'),
-                'username' => env('REDIS_USERNAME'),
                 'password' => env('REDIS_PASSWORD'),
                 'port' => env('REDIS_PORT', '6379'),
                 'database' => env('REDIS_DB', '0'),
             ],
         ],
 
-        'cache' => [
-            'url' => env('REDIS_URL'),
+        'default' => [
+            'scheme' => 'tls', //Fixed AWS Redis connection
+            // 'url' => env('REDIS_URL'),
+            // 'username' => env('REDIS_USERNAME'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
+        ],
+
+        'cache' => [
+            // 'url' => env('REDIS_URL'),
+            // 'username' => env('REDIS_USERNAME'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
